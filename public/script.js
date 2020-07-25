48cb0c808bc07a07cb302c4a4bddb5f684878946
Glitch (hello-express) <none>
2020-07-25 11:08:05 +0000

    🌺🎊 Checkpoint ./public/script.js:19780202/5505 ./public/index.html:19780202/46

--- public/script.js
+++ public/script.js
@@ -2,20 +2,41 @@ navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia;
 
-if (navigator.getUserMedia) {
-   navigator.getUserMedia({ audio: true, video: { width: 800, height: 600 } },
-      function(stream) {
-         
-         var video = document.querySelector('video');
-         video.srcObject = stream;
-         video.onloadedmetadata = function(e) {
-           video.play();
-         };
-      },
-      function(err) {
-         console.log("The following error occurred: " + err.name);
-      }
-   );
-} else {
-   console.log("getUserMedia not supported");
+// check if streaming requests with fetch API is available
+const supportsRequestStreams = !new Request('', {
+  body: new ReadableStream(),
+  method: 'POST',
+}).headers.has('Content-Type');
+
+
+const channel = Math.random();
+
+if (!navigator.getUserMedia) {
+  console.error('This browser doesn\'t support navigator.getUserMedia()')
 }
+navigator.getUserMedia({ audio: true, video: { width: 800, height: 600 } },
+  (stream) => {
+    fetch(`/send?channel=${channel}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
+      body: stream,
+      allowHTTP1ForStreamingUpload: true,
+    });
+
+    fetch(`/receive?channel=${channel}`).then(async res => {
+      const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) return;
+        var video = document.querySelector('video');
+        video.srcObject = value;
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+      }
+    });
+  },
+  (error) => {
+    console.error(error);
+  }
+);
